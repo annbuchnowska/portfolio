73ca4f86b83769ac1078a621d8c322804592ec18
cheri-x <buchnowska.anna@gmail.com>
2016-09-04 17:46:44 +0200

    add -what- content

--- js/main.js
+++ js/main.js
@@ -16,20 +16,21 @@ $(document).ready(function() {
       activeHeader: "iconUp" // custom icon class
   };
 	$( "#accordion" ).accordion({
-		header: "a",
+		header: "a.sectionTitle",
 		active: false,
 		collapsible: true,
 		animate: 1000,
     icons: icons
 	});
+  $('#intro-slider').flexslider({
+      animation: 'fade',
+      controlNav: false,
+      slideshowSpeed: 4000,
+   });
 });
+
 
 
 // $("a").click(function(){
 // 	$('a').not(".ui-state-active").addClas(".hide");
 // });
-
-//contact form
-new ContactForm('#contact-form', {
-    endpoint: './process.php'
-});
