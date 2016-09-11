72a7ceb9148ca24248192face5cfa0a9e6a3b9af
cheri-x <buchnowska.anna@gmail.com>
2016-09-11 19:10:52 +0200

    fix scrool - chrome

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-//disable scrolling
-$('html, body').css({
-  'overflow': 'hidden',
-  'height': '100%'
-});
-// to restore
-// $('html, body').css({
-//     'overflow': 'auto',
-//     'height': 'auto'
-// });
-
 // accordion-ui
 $(document).ready(function() {
   var icons = {
@@ -27,4 +16,37 @@ $(document).ready(function() {
     controlNav: false,
     slideshowSpeed: 4000,
   });
+});
+
+
+// contact-form
+$(function() {
+	var form = $('#ajax-contact');
+	var formMessages = $('#form-messages');
+	$(form).submit(function(e) {
+		e.preventDefault();
+		var formData = $(form).serialize();
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+			$(formMessages).text(response);
+			$('#name').val('');
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+	});
 });
